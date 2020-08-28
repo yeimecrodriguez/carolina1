4ea5fd2188c3ba9c0ff7fd5933b2fd0f6db02fab
carolina <yeimecrodriguez@ucundinamarca.edu.co>
2020-08-27 19:55:26 -0500

    cambio1

---
+++ ejercicio.java
@@ -0,0 +1,26 @@
+package g2ejercicio2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Karoline
+ */
+public class G2EJERCICIO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner (System.in);
+        int radio,areacir,areacuadrado,areasomb;
+        System.out.println("Ingrese el radio del circulo:");
+        radio = teclado.nextInt();
+        areacir = (int) (Math.PI/(Math.pow(radio,2)));
+        areacuadrado = (int) (2*(Math.pow(radio,2)));
+        areasomb = (int) ((Math.PI*(Math.pow(radio, 2))) - (2*(Math.pow(radio, 2))));
+        System.out.println("el area de la parte sombreada es: "+areasomb);
+    }
+    
+}
